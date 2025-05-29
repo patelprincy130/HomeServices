6349b15f9620c5418090095054a96b0f9080f92d
Princy Patel <patelprincy130@gmail.com>
2025-05-29 10:58:02 -0400

    Change in index.js with cors

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 import userRoute from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
 import protectedRoute from "./routes/protected.route.js";
+import cors from 'cors';
+
 
 dotenv.config();
 
@@ -11,6 +13,7 @@ const app=express();
 const port=process.env.PORT || 3000;
 const mongo_uri=process.env.MONGO_URI;
 
+app.use(cors());
 //to receive json data from body
 app.use(express.json());
 app.use(cookieParser());
